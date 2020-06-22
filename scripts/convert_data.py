819b7777183fefe2e54cae5d38a22120ff2b9786
qovaxx <m-tan.rx@yandex.ru>
2020-06-22 08:58:47 +0300

    WIP(all): checkpoint before big refactoring

--- scripts/convert_data.py
+++ scripts/convert_data.py
@@ -9,4 +9,6 @@ from src.psga.settings import (
 writer = TIFFWriter(str(PROCESSED_DIRPATH / KAGGLE_DATASET_NAME), quality=90, tile_size=512)
 adapter = PSGADataAdapter(str(RAW_DIRPATH / KAGGLE_DATASET_NAME), writer,
                           verbose=True, layer=0, crop_tissue_roi=True)
-adapter.convert(processes=10)
+# The number of processes is memory dependent.
+# At zero layer, on average, one process consumes 20 gigabytes, at the peak up to 60
+adapter.convert(processes=4)
