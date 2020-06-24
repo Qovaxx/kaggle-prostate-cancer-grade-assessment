d58412de03375923b3678255daa8c8850513980f
qovaxx <m-tan.rx@yandex.ru>
2020-06-24 13:57:50 +0300

    WIP(all): refactor project structure

--- trash4.py
+++ trash4.py
@@ -1,13 +1,14 @@
 from skimage.io import MultiImage
 
+from src.psga.tools.preprocessing import
 
 
 
+slide = MultiImage("/data/raw/prostate-cancer-grade-assessment/train_images/5c023def11d24939459afd3e3cb69620.tiff")
 
-
-image = MultiImage("/data/raw/prostate-cancer-grade-assessment/train_images/5c023def11d24939459afd3e3cb69620.tiff")
-
-
+image0 = slide[0]
+image1 = slide[1]
+image2 = slide[2]
 
 
 
