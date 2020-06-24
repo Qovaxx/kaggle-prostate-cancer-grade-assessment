f32b4563955f30dc7372748203f187cfd69c1d5b
qovaxx <m-tan.rx@yandex.ru>
2020-06-24 16:46:31 +0300

    WIP(all): refactoring preprocessing methods

--- src/psga/tools/preprocessing.py
+++ src/psga/tools/preprocessing.py
@@ -1,92 +1,81 @@
-from dataclasses import dataclass
+from skimage import morphology
 from typing import (
     Optional,
-    Tuple,
-    NoReturn
+    Tuple
 )
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def remove_pen_marks(image: np.ndarray, kernel_size: Tuple[int, int] = (5, 5), max_tissue_value: int = 220,
-                     red_left_shift: int = 50, background_value: int = 255) -> Tuple[np.ndarray, np.ndarray]:
-    corrected = image.copy()
-    gray = cv2.cvtColor(corrected, code=cv2.COLOR_RGB2GRAY)
-    non_background_mask = np.where(gray < max_tissue_value, 1, 0).astype(np.uint8)
-    plt.imshow(non_background_mask)
+def show(image):
+    plt.figure()
+    plt.imshow(image)
     plt.show()
 
-    # Mask pen marks pixels
-    red_pixels = np.reshape(corrected[:, :, 0], (-1,))
-    red_pixels = red_pixels[np.where(red_pixels < background_value)[0]]
-    pen_marks_mask = (corrected[:, :, 0] < np.median(red_pixels) - red_left_shift).astype(np.uint8)
 
-    # When computing the pen mark mask, some tissue gets masked as well,
-    # thus needing to erode the mask to get rid of it. Then some dilatation is
-    # applied to capture the "edges" of the "gradient-like"/non-uniform pen marks
-    kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=kernel_size) # kernel size selected for level 1
+def crop_rectangle(image: np.ndarray, mask: Optional[np.ndarray], rectangle: Tuple[Tuple, Tuple, float],
+                   border_value: int) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+    box = cv2.boxPoints(box=rectangle)
+    width = int(rectangle[1][0])
+    height = int(rectangle[1][1])
+    reference_bbox = np.array([[0, height - 1], [0, 0], [width - 1, 0], [width - 1, height - 1]], dtype=np.float32)
+    transformation_matrix = cv2.getPerspectiveTransform(src=box, dst=reference_bbox)
+
+    crop = cv2.warpPerspective(image, M=transformation_matrix, dsize=(width, height),
+                               flags=cv2.INTER_LANCZOS4, borderValue=tuple([border_value] * 3))
+    if mask is not None:
+        mask = cv2.warpPerspective(mask, M=transformation_matrix, dsize=(width, height),
+                                   flags=cv2.INTER_NEAREST, borderValue=0)
+
+    return crop, mask
+
+
+def crop_min_roi(image: np.ndarray, mask: Optional[np.ndarray] = None, min_background_value: int = 255
+                 ) -> Tuple[np.ndarray, Optional[np.ndarray], Tuple[Tuple, Tuple, float]]:
+    if mask is not None:
+        assert image.shape[:2] == mask.shape
+
+    gray = cv2.cvtColor(image, code=cv2.COLOR_RGB2GRAY)
+    _, roi = cv2.threshold(gray, thresh=min_background_value - 1, maxval=image.max(), type=cv2.THRESH_BINARY_INV)
+    contours, _ = cv2.findContours(roi, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+    rectangle = cv2.minAreaRect(points=np.concatenate(contours))
+    cropped_image, cropped_mask = crop_rectangle(image, mask, rectangle, border_value=min_background_value)
+
+    return cropped_image, cropped_mask, rectangle
+
+
+def remove_pen_marks(image: np.ndarray, mask: Optional[np.ndarray], kernel_size: Tuple[int, int],
+                     max_tissue_value: int = 220, red_left_shift: int = 50,
+                     background_value: int = 255) -> Tuple[np.ndarray, Optional[np.ndarray], np.ndarray]:
+    if mask is not None:
+        assert image.shape[:2] == mask.shape
+
+    gray = cv2.cvtColor(image, code=cv2.COLOR_RGB2GRAY)
+    non_gray_mask = np.where(gray < max_tissue_value, 1, 0).astype(np.uint8)
+
+    # Mask pen marks pixels
+    red_pixels = np.reshape(image[:, :, 0], (-1,))
+    red_pixels = red_pixels[np.where(red_pixels < background_value)[0]]
+    pen_marks_mask = (image[:, :, 0] < np.median(red_pixels) - red_left_shift).astype(np.uint8)
+
+    # Some tissue gets masked as well, thus needing to erode the mask to get rid of it.
+    # Then some dilatation is applied to capture the "edges" of the "gradient-like"/non-uniform pen marks
+    kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=kernel_size)
     pen_marks_mask = cv2.erode(pen_marks_mask, kernel, iterations=3)
     pen_marks_mask = cv2.dilate(pen_marks_mask, kernel, iterations=5)
+    tissue_mask = non_gray_mask * (1 - pen_marks_mask)
 
-    pen_marks_mask = 1 - pen_marks_mask
-    mask = non_background_mask * pen_marks_mask
+    morphology.remove_small_holes(tissue_mask.astype(np.bool), area_threshold=1000, connectivity=1, in_place=True)
+    tissue_mask = cv2.dilate(tissue_mask, kernel, iterations=2)
+    tissue_mask = cv2.erode(tissue_mask, kernel, iterations=4)
+    tissue_mask = cv2.dilate(tissue_mask, kernel, iterations=2)
 
-    # Fill some gaps/holes in the tissue
-    mask = cv2.morphologyEx(mask, op=cv2.MORPH_CLOSE, kernel=kernel, iterations=1)
-    mask = cv2.dilate(mask, kernel, iterations=1)
-    contours, _ = cv2.findContours(mask, mode=cv2.RETR_CCOMP, method=cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        cv2.drawContours(mask, [contour], 0, 1, -1)
+    corrected_image = image * tissue_mask[..., np.newaxis] + background_value
+    corrected_mask = mask * tissue_mask if mask is not None else None
 
-    mask = cv2.erode(mask, kernel, iterations=3)
-    mask = cv2.dilate(mask, kernel, iterations=1)
-    mask = cv2.erode(mask, kernel, iterations=2)
-    corrected = corrected * mask[..., np.newaxis] + background_value
-    return corrected, mask
+    return corrected_image, corrected_mask, tissue_mask
 
 
-@dataclass
-class BBox(object):
-    x1: int
-    y1: int
-    x2: int
-    y2: int
-    shape: Tuple[int, int]
+def convert_to_atlas(image: np.ndarray, mask: Optional[np.ndarray])
 
-    def rescale(self, target_shape: Tuple[int, int]):
-        x_scale = target_shape[0] / self.shape[0]
-        y_scale = target_shape[1] / self.shape[1]
-        return BBox(x1=int(self.x1 * x_scale),
-                    y1=int(self.y1 * y_scale),
-                    x2=int(self.x2 * x_scale),
-                    y2=int(self.y2 * y_scale),
-                    shape=target_shape)
-
-
-class NoneWhiteROICropper(object):
-
-    def __init__(self, min_background_value: int = 255) -> NoReturn:
-        self._min_background_value = min_background_value
-        self._bbox: Optional[BBox] = None
-
-    @property
-    def bbox(self) -> Optional[BBox]:
-        return self._bbox
-
-    def __call__(self, image: np.ndarray, mask: Optional[np.ndarray] = None,
-                 bbox: Optional[BBox] = None) -> Tuple[np.ndarray, Optional[np.ndarray]]:
-        if mask is not None:
-            assert image.shape[:2] == mask.shape[:2]
-
-        if bbox is None:
-            height_indices = (image.min(axis=(1, 2)) < self._min_background_value).nonzero()[0]
-            width_indices = (image.min(axis=(0, 2)) < self._min_background_value).nonzero()[0]
-            bbox = BBox(width_indices[0], width_indices[-1], height_indices[0], height_indices[-1], image.shape[:2])
-        self._bbox = bbox
-        crop_it = lambda x: x[bbox.x2: bbox.y2, bbox.x1: bbox.y1]
-
-        image = crop_it(image)
-        if mask is not None:
-            mask = crop_it(mask)
-        return image, mask
--- trash4.py
+++ trash4.py
@@ -1,20 +1,35 @@
 from skimage.io import MultiImage
-
-from src.psga.tools.preprocessing import
-
+import matplotlib.pyplot as plt
 
 
-slide = MultiImage("/data/raw/prostate-cancer-grade-assessment/train_images/5c023def11d24939459afd3e3cb69620.tiff")
+from src.psga.tools.preprocessing import crop_min_roi, remove_pen_marks
 
-image0 = slide[0]
-image1 = slide[1]
-image2 = slide[2]
+def show(image):
+    plt.figure()
+    plt.imshow(image)
+    plt.show()
+
+#5c023def11d24939459afd3e3cb69620#
+name = "308af8bc571b7c2e743a35d7489e4e32"
+slide_image = MultiImage(f"/data/raw/prostate-cancer-grade-assessment/train_images/{name}.tiff")
+slide_mask = MultiImage(f"/data/raw/prostate-cancer-grade-assessment/train_label_masks/{name}_mask.tiff")
+
+
+image = slide_image[1]
+mask = slide_mask[1][..., 0]
+
+image_roi, mask_roi, rectangle_roi = crop_min_roi(image, mask)
+image_clear, mask_clear, clear_mask = remove_pen_marks(image_roi, mask_roi, kernel_size=(5, 5))
 
 
 
 
+show(image_roi)
+show(mask_roi)
 
-
+show(image_clear)
+show(mask_clear)
+show(clear_mask)
 
 
 a = 4
