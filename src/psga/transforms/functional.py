a8f542328db07c1be2e3e475e0b36dfb1cf6575a
qovaxx <m-tan.rx@yandex.ru>
2020-06-29 12:04:28 +0300

    WIP(all): fix chunked warpAffine cropping

--- src/psga/transforms/functional.py
+++ src/psga/transforms/functional.py
@@ -7,9 +7,35 @@ from .entity import (
     Rectangle
 )
 
+import matplotlib.pyplot as plt
+def show(image):
+    plt.figure()
+    plt.imshow(image)
+    plt.show()
 
-def crop_bbox(image: np.ndarray, bbox: BBox) -> np.ndarray:
-    return image[bbox.y: bbox.y + bbox.height, bbox.x: bbox.x + bbox.width]
+
+def crop_bbox(image: np.ndarray, bbox: BBox, with_padding: bool = False) -> np.ndarray:
+    x_max = bbox.x + bbox.width
+    y_max = bbox.y + bbox.height
+    pads = (bbox.x, bbox.y, image.shape[1]-x_max, image.shape[0]-y_max)
+    correct_pads = len(list(filter(lambda x: x>=0, pads)))
+
+    if with_padding is False or correct_pads == len(pads):
+        return image[bbox.y: bbox.y + bbox.height, bbox.x: bbox.x + bbox.width]
+
+    else:
+        pads = list(map(lambda x: 0 if x > 0 else abs(x), pads))
+        pad_width = [(pads[1], pads[3]), (pads[0], pads[2])]
+        constant_values = 0
+        if len(image.shape) == 3:
+            pad_width.append((0, 0))
+            constant_values = 255
+
+        padded = np.pad(image, pad_width=pad_width, mode="constant", constant_values=constant_values)
+        return padded[
+               pads[1] + bbox.y: pads[1] + bbox.y + bbox.height,
+               pads[0] + bbox.x: pads[0] + bbox.x + bbox.width
+               ]
 
 
 def crop_slice(image: np.ndarray, slice: Slice2D) -> np.ndarray:
@@ -35,7 +61,6 @@ def crop_rectangle(image: np.ndarray, rectangle: Rectangle, is_mask: bool = Fals
         flags = cv2.INTER_LANCZOS4
         border_value = (255, 255, 255)
 
-
     image = cv2.warpAffine(src=image, M=transform, dsize=(enlarged_width, enlarged_height),
                            flags=flags, borderMode=cv2.BORDER_CONSTANT, borderValue=border_value)
 
@@ -46,5 +71,5 @@ def crop_rectangle(image: np.ndarray, rectangle: Rectangle, is_mask: bool = Fals
                                   width=rectangle.width,
                                   height=rectangle.height,
                                   angle=0)
-    image = crop_bbox(image, bbox=rotated_rectangle.to_d4_bbox())
+    image = crop_bbox(image, bbox=rotated_rectangle.to_d4_bbox(), with_padding=True)
     return image
