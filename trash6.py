2e57a0586efaf248708934510c9754a2e8bf4471
qovaxx <m-tan.rx@yandex.ru>
2020-07-03 14:06:41 +0300

    Feature(evaluation): add torch cohen_kappa_score

--- trash6.py
+++ trash6.py
@@ -4,19 +4,15 @@ from sklearn.metrics import cohen_kappa_score, confusion_matrix
 import  src.psga.train.evaluation.functional as F
 
 
-input = [1,2,4,1,2,3,4,5,0,1]
-target = [2,2,2,1,2,3,4,5,0,1]
-labels = [1, 2, 3]
-sample_weights = [2,1,1,2,1,1,3,1,1,1]
-sklearn_cm = confusion_matrix(target, input, labels=None, sample_weight=sample_weights)
+input = [2,2,2,3,4,5,5,5,5,5]
+target = [2,2,2,3,2,1,1,1,1,3]
+
+sklearn_score = cohen_kappa_score(input, target, weights="quadratic")
 
 
 input = torch.tensor(input)
 target = torch.tensor(target)
-
-torch_cm = F.confusion_matrix(input, target, labels=None, sample_weight=torch.tensor(sample_weights)).numpy()
-
-
+torch_score = F.cohen_kappa_score(input, target, weights="quadratic").item()
 
 
 a = 4
